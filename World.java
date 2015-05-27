a9807cb7ec4d315a6a205203ef713d3b1e34958e
jelliott37 <jessee8991@gmail.com>
2015-05-27 09:25:57 -0400

    stuff

--- World.java
+++ World.java
@@ -1,3 +1,16 @@
 public class World{
     //handles the 2d array that is the gamespace
+    private char[][] map;
+    public world(int n){ //precondition: n is odd
+	map = new char[n][n];
+    }
+    public void generate(){ //creates map
+	for(int r = 0; r < map.length; r++){
+	    if(r == 0 || r == map.length - 1){
+		for(int c = 0; c < map.length; c++){
+		    if
+		}
+	    }
+	}
+    }
 }

25d3c93b952b32ee58e9c34adf62a7c4409cb6c1
jelliott37 <jessee8991@gmail.com>
2015-05-28 08:54:33 -0400

    stuff

--- World.java
+++ World.java
@@ -1,16 +1,33 @@
 public class World{
     //handles the 2d array that is the gamespace
     private char[][] map;
+    private int sideLength;
     public world(int n){ //precondition: n is odd
 	map = new char[n][n];
+	sideLength = n;
     }
     public void generate(){ //creates map
-	for(int r = 0; r < map.length; r++){
-	    if(r == 0 || r == map.length - 1){
-		for(int c = 0; c < map.length; c++){
-		    if
+	for(int r = 0; r < sideLength; r++){
+		for(int c = 0; c < sideLength; c++){
+		    if(r == 0 || r == sideLength - 1){
+			if(c != sideLength/2 && 
+			   c != sideLength/2 + 1 && 
+			   c != sideLength/2 + 2){
+			    
+			}
+		    }
 		}
+	}    
+    }
+    public String toString()
+    {
+	String s = "[2J\n";
+	for (int y=0;y<sideLength;y++)
+	    {
+		for (int x=0;x<maxX;x++)
+		    s = s +board[x][y];
+		s=s+"\n";
 	    }
-	}
+	return s;
     }
 }

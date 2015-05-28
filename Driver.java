db97ee6c8bfe6c491578cc810fba06a0215da31f
jelliott37 <jessee8991@gmail.com>
2015-05-28 09:26:53 -0400

    stuff

--- Driver.java
+++ Driver.java
@@ -1,6 +1,8 @@
 public class Driver{
     
     public static void main(String[] args){
-	
+	World w = new World(25);
+	w.generate();
+	System.out.println(w);
     }
 }
--- World.java
+++ World.java
@@ -2,30 +2,65 @@ public class World{
     //handles the 2d array that is the gamespace
     private char[][] map;
     private int sideLength;
-    public world(int n){ //precondition: n is odd
+    public World(int n){ //precondition: n is odd
 	map = new char[n][n];
 	sideLength = n;
     }
     public void generate(){ //creates map
-	for(int r = 0; r < sideLength; r++){
-		for(int c = 0; c < sideLength; c++){
-		    if(r == 0 || r == sideLength - 1){
-			if(c != sideLength/2 && 
-			   c != sideLength/2 + 1 && 
-			   c != sideLength/2 + 2){
-			    
-			}
+	generateBorders();
+	
+    }
+    public void generateBorders(){ //creates borders and fills with blankspace
+	for(int c = 0; c < sideLength; c++){
+	    for(int r = 0; r < sideLength; r++){
+		if(c == 0 || c == sideLength - 1){
+		    if(r != sideLength/2 - 1 && 
+		       r != sideLength/2 && 
+		       r != sideLength/2 + 1){
+			map[r][c] = '_';
+		    }
+		    else{
+			map[r][c] = ' ';
 		    }
 		}
+		else if(r == 0 || r == sideLength - 1){
+		    if(c != sideLength/2 - 1 && 
+		       c != sideLength/2 && 
+		       c != sideLength/2 + 1){
+			map[r][c] = '|';
+		    }
+		    else{
+			map[r][c] = ' ';
+		    }
+		}
+		else{
+		    map[r][c] = ' ';
+		}
+	    }
 	}    
+    }
+    public void preset1(){
+	
+    }
+    public void preset2(){
+
+    }
+    public void preset3(){
+
+    }
+    public void presetBoss(){
+	
+    }
+    public void generateChests(){
+	
     }
     public String toString()
     {
-	String s = "[2J\n";
+	String s = "\033\143";
 	for (int y=0;y<sideLength;y++)
 	    {
-		for (int x=0;x<maxX;x++)
-		    s = s +board[x][y];
+		for (int x=0;x<sideLength;x++)
+		    s = s + map[x][y];
 		s=s+"\n";
 	    }
 	return s;

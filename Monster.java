57518b212d2843f8b249a20475729759ee7839f7
Revalucien <thundertrispear@gmail.com>
2015-06-14 01:55:43 -0400

    more monster

--- Monster.java
+++ Monster.java
@@ -14,14 +14,26 @@ public class Monster extends Entity{
 	int mcy = this.getY();
 	if (Math.sqrt((pcx-mcx)*(pcx-mcx)+(pcy-mcy)*(pcy-mcy))>8){
 	    int dir = rand.nextInt(8);
-	    if (dir == 0){w.move(this, this.getX()-1, this.getY()-1);}
-	    if (dir == 1){w.move(this, this.getX(), this.getY()-1);}
-	    if (dir == 2){w.move(this, this.getX()+1, this.getY()-1);}
-	    if (dir == 3){w.move(this, this.getX()+1, this.getY());}
-	    if (dir == 4){w.move(this, this.getX()+1, this.getY()+1);}
-	    if (dir == 5){w.move(this, this.getX(), this.getY()+1);}
-	    if (dir == 6){w.move(this, this.getX()-1, this.getY()+1);}
-	    if (dir == 7){w.move(this, this.getX()-1, this.getY());}
+	    if (dir == 0){w.move(this, this.setX()-1, this.setY()-1);}
+	    if (dir == 1){w.move(this, this.setX(), this.setY()-1);}
+	    if (dir == 2){w.move(this, this.setX()+1, this.setY()-1);}
+	    if (dir == 3){w.move(this, this.setX()+1, this.setY());}
+	    if (dir == 4){w.move(this, this.setX()+1, this.setY()+1);}
+	    if (dir == 5){w.move(this, this.setX(), this.setY()+1);}
+	    if (dir == 6){w.move(this, this.setX()-1, this.setY()+1);}
+	    if (dir == 7){w.move(this, this.setX()-1, this.setY());}
+	} else if(Math.abs(mcx-pcx)!=1 && Math.abs(mcy-pcy)!=1){
+	    if (mcx>pcx && mcy>pcy){w.move(this, this.setX()-1, this.setY()-1);}
+	    if (mcx>pcx && mcy=pcy){w.move(this, this.setX()-1, this.setY());}
+	    if (mcx>pcx && mcy<pcy){w.move(this, this.setX()-1, this.setY()+1);}
+	    if (mcx=pcx && mcy<pcy){w.move(this, this.setX(), this.setY()+1);}
+	    if (mcx<pcx && mcy<pcy){w.move(this, this.setX()+1, this.setY()+1);}
+	    if (mcx<pcx && mcy=pcy){w.move(this, this.setX()+1, this.setY());}
+	    if (mcx<pcx && mcy>pcy){w.move(this, this.setX()+1, this.setY()+1);}
+	    if (mcx=pcx && mcy>pcy){w.move(this, this.setX(), this.setY()-1);}
+	} else if (Math.abs(mcx-pcx)=1 || Math.abs(mcy-pcy)=1){
+	    //attack player
 	}
-    }
+
+	public void
 }
